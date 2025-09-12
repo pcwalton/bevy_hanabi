7453312b247bba989b01a5fdc03ec93860ca2d7d
Patrick Walton <pcwalton@mimiga.net>
2025-09-12 10:27:01 -0700

    Compile fixes

--- src/render/effect_cache.rs
+++ src/render/effect_cache.rs
@@ -7,7 +7,7 @@ use std::{
 use bevy::{
     asset::Handle,
     ecs::{component::Component, resource::Resource},
-    log::{info, trace, warn},
+    log::{trace, warn},
     platform::collections::HashMap,
     render::{render_resource::*, renderer::RenderDevice},
     utils::default,
